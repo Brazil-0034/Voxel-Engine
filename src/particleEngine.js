636a15fe9b8f7f1a5cc9798541439dcecf440c6b
Austin <66288732+nowaythisworks@users.noreply.github.com>
2023-08-16 18:35:49 -0700

    Advanced Lighting System

---
+++ src/particleEngine.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three';
+
+export class Particle {
+    instanceBufferPosition
+    position
+    scale
+    rotation
+    direction
+    color
+    speed
+    lifetime
+
+    constructor(particleMesh, position, direction, color, speed) {
+        this.position = position;
+        this.scale = new THREE.Vector3(1,1,1);
+        this.rotation = new THREE.Quaternion();
+        this.rotation.setFromUnitVectors(new THREE.Vector3(0,(Math.random() < 0.5 ? 1 : -1), 0), direction.clone().normalize());
+        this.direction = direction.multiplyScalar(speed);
+        this.color = color;
+        this.speed = speed;
+        this.instanceBufferPosition = particleMesh.addParticle(this);
+        this.lifetime = 0;
+    }
+}
+
+export class ParticleMesh extends THREE.InstancedMesh {
+    bufferPosition
+    maxCount
+    particlesList
+    
+    constructor(geometry, material, count) {
+        super(geometry, material, count)
+        this.bufferPosition = 0;
+        this.maxCount = count;
+        this.particlesList = [];
+    }
+
+    addParticle(particle) {
+        if (this.bufferPosition >= this.maxCount) {
+            this.bufferPosition = 0;
+        }
+        this.setMatrixAt(this.bufferPosition, new THREE.Matrix4().makeTranslation(particle.position.x, particle.position.y, particle.position.z))
+        this.setColorAt(this.bufferPosition, particle.color);
+        this.instanceColor.needsUpdate = true;
+        this.particlesList.push(particle);
+        this.bufferPosition += 1;
+
+        return this.bufferPosition - 1;
+    }
+
+    update(delta) {
+        for (let i = 0; i < this.particlesList.length; i++)
+        {
+            let particle = this.particlesList[i];
+            particle.position.x += particle.direction.x * delta;
+            particle.position.y += (particle.direction.y - (particle.lifetime * 1.5)) * delta;
+            particle.position.z += particle.direction.z * delta;
+            particle.lifetime += 1;
+
+            // delay scaling until after a little while
+            if (particle.lifetime < 1000 * delta) particle.scale.set(1,1,1);
+
+            // rotate on x and z axis based on lifetime
+            particle.rotation.setFromEuler(new THREE.Euler(particle.lifetime * 0.01, 0, particle.lifetime * 0.01));
+
+            // rescale with delta
+            this.setMatrixAt(particle.instanceBufferPosition, new THREE.Matrix4().compose(particle.position, particle.rotation, particle.scale.multiplyScalar(1 - (delta * 1.5))));
+        }
+        this.instanceMatrix.needsUpdate = true;
+    }
+}
